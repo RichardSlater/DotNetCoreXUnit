f5694fa1df58402ab0e330176ff09c9c660c1198
Richard Slater <github@richard-slater.co.uk>
2016-07-21 22:04:08 +0100

    Added first test.

---
+++ src/StringReverser/Engine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StringReverser
+{
+    public class Engine
+    {
+        public string Reverse(string original)
+        {
+            return new string(original.Reverse().ToArray());
+        }
+    }
+}
---
+++ src/StringReverserTests/SwapsCharacters.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using StringReverser;
+
+namespace StringReverserTests
+{
+    public class SwapsCharacters
+    {
+        [Fact]
+        public void TwoDigitSwap()
+        {
+            const string original = "ab";
+            var reverser = new Engine();
+            var result = reverser.Reverse(original);
+            Assert.Equal("ba", result);
+        }
+    }
+}
